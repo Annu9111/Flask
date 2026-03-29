f42104b6a3e7118c03911eb131e324ef38607c62
Annu9111 <aksoni911190@gmail.com>
2026-03-29 10:43:22 +0530

    flask

--- app.py
+++ app.py
@@ -6,4 +6,12 @@ app=Flask(__name__)
 def home():
     return "hello,Flask!"
 
+@app.route("/about")       #statics routes
+def about():
+    return "this is About Page"
+
+@app.route("/user/<name>")
+def user(name):
+    return f"hello {name}"
+
 app.run(debug=True)
