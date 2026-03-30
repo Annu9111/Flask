c3142d9d7c2c81ed1301e16739aa37baa7fb6e75
Annu9111 <aksoni911190@gmail.com>
2026-03-30 14:18:49 +0530

    flask

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
 from flask import Blueprint, render_template
+from .models import User
+from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
     return render_template('index.html')
+
+@main.route('/add/<name>')
+def add_user(name):
+    user = User(name=name)
+    db.session.add(user)
+    db.session.commit()
+    return f"{name} added!"
+
+@main.route('/users')
+def get_users():
+    users = User.query.all()
+    return "<br>".join([user.name for user in users])
